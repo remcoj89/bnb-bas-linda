d37ed347a08cb38079f21d9877d636c90e7894dd
remco jonk <remcojonk@hotmail.com>
2023-03-02 14:03:52 +0100

    Added Create and read in the controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,28 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Bookings.all
+    @bookings = Booking.all
+  end
+
+  def show
+    find_by_id
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+  private
+
+  def find_by_id
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:name, :start_date, :end_date)
   end
 end
